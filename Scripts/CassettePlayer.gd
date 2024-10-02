4afd58a387e484af417dc5380db156e622634a2c
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2024-10-02 16:20:11 +0100

    Improved cassette player spin mechanic

--- Scripts/CassettePlayer.gd
+++ Scripts/CassettePlayer.gd
@@ -1,5 +1,7 @@
 extends Grabbable
 
+var speed : Vector2
+
 func _ready() -> void:
 	pass
 
@@ -7,7 +9,18 @@ func _process(delta: float) -> void:
 	#grabbed(Vector2(100.0 * randf_range(0.7, 1.3) * delta, 100.0 * randf_range(0.7, 1.3) * delta));
 	pass
 
+func _physics_process(delta):
+	if abs(speed.x) > 0.05 or abs(speed.y) > 0.05:
+		rotate(Vector3(0.0, 1.0, 0.0), speed.x * delta)
+		rotate(Vector3(1.0, 0.0, 0.0), speed.y * delta)
+		speed.x *= 0.95
+		speed.y *= 0.95
+	else:
+		speed.x = 0.0
+		speed.y = 0.0
+	print(speed)
+
 func grabbed(relative : Vector2):
-	print("relative: " + str(relative.x) + ", " + str(relative.y))
-	rotate(Vector3(0.0, 1.0, 0.0), relative.x * 0.01)
-	rotate(Vector3(1.0, 0.0, 0.0), relative.y * 0.01)
+	speed += relative * 0.01
+	#rotate(Vector3(0.0, 1.0, 0.0), relative.x * 0.01)
+	#rotate(Vector3(1.0, 0.0, 0.0), relative.y * 0.01)
--- Scripts/Grabbable.gd
+++ Scripts/Grabbable.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends CharacterBody3D
 class_name Grabbable
 
 func grabbed(relative : Vector2):
